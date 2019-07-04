accaaf9bf1f1df6d4a99552af959405c1f9faca1
zendus <uzendujmike@gmail.com>
2019-07-04 13:44:53 +0100

    defined displayCart()

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const fs = require('fs')
 
 //product array
 const products = [
-     {"id" : 1, "name" : "milo", "price" : 350},
-     {"id" : 2, "name" : "bread", "price" : 150 },
-     {"id" : 3, "name" : "milk", "price" : 50},
-     {"id" : 4, "name" : "okpa", "price" : 150},
-     {"id" : 5, "name" : "beans", "price" : 200}
+     { "id": 1, "name": "milo", "price": 350 },
+     { "id": 2, "name": "bread", "price": 150 },
+     { "id": 3, "name": "milk", "price": 50 },
+     { "id": 4, "name": "okpa", "price": 150 },
+     { "id": 5, "name": "beans", "price": 200 }
 ]
 
 //cart array
 const cart = [];
 
 //page header
-function header (title, msg){
+function header(title, msg) {
      console.clear();
      console.log("========================================================")
      console.log(title)
@@ -26,27 +26,27 @@ function header (title, msg){
 }
 
 //home page
-function home (msg){
+function home(msg) {
      header('WELCOME TO SHOPU - home', msg)
      console.log("1.", "View products");
      console.log("2.", "View cart");
      console.log("3.", "View checkout");
      console.log();
      prompt.question("Please Select an option: ", (opt) => {
-          if(opt == 1){
+          if (opt == 1) {
                displayProducts('');
-          }else if(opt == 2){
+          } else if (opt == 2) {
                displayCart('');
-          }else if(opt == 3){
+          } else if (opt == 3) {
                checkout('');
-          }else {
+          } else {
                home('Invalid input ! 🙃')
-          }      
+          }
      });
 }
 
 //product page --------Mich
-function displayProducts (msg){
+function displayProducts(msg) {
      header('OUR PRODUCTS - list of available products', msg)
      console.table(products)
      console.log();
@@ -56,57 +56,76 @@ function displayProducts (msg){
      console.log("4.", "Go to Home");
      console.log();
      prompt.question("Please Select an option ", (opt) => {
-          if(opt == 1){
+          if (opt == 1) {
                displayProducts('');
-          }else if(opt == 2){
+          } else if (opt == 2) {
                displayCart('');
-          }else if(opt == 3){
+          } else if (opt == 3) {
                checkout('');
-          }else {
+          } else {
                displayProducts("Invalid input ! 🙃")
           }
      });
 }
 
 //cart page --------Mich
-function displayCart(msg){
+function displayCart(msg) {
      header('YOUR CART', msg)
      console.table(cart);
+     console.log();
+     console.log("1.", "Go to products");
+     console.log("2.", "Add to cart");
+     console.log("3.", "View checkout");
+     console.log("4.", "Go to Home");
+     console.log();
+     prompt.question("Please Select an option ", (opt) => {
+          if (opt == 1) {
+               displayProducts('');
+          } else if (opt == 2) {
+               addToCart('');
+          } else if (opt == 3) {
+               checkout('');
+          } else if (opt == 4) {
+               home('');
+          } else {
+               displayProducts("Invalid input ! 🙃")
+          }
+     });
 }
 
 //checkout page --------Jerry
-function checkout(msg){
+function checkout(msg) {
      header('CHECKOUT', msg)
 }
 
 //add product to cart --------Victor
-function addToCart(msg){
+function addToCart(msg) {
      header('ADD TO CART', msg)
 }
 
 //remove product from Cart --------Victor
-function removeFromCart(msg){
+function removeFromCart(msg) {
      header('REMOVE FROM CART -  remove an item', msg)
 }
 
 // cart product added successfuly --------Hope
-function addedCartItem(){
+function addedCartItem() {
      header('PRODUCT ADDED SUCCESSFULLY !', msg)
 }
 
 // cart product removed successfuly --------Hope
-function removedCartItem(msg){
+function removedCartItem(msg) {
      header('PRODUCT REMOVED SUCCESSFULLY !', msg)
 }
 
 
 //payment card details --------Jerry
-function cardDetails(msg){
+function cardDetails(msg) {
      header('ENTER YOUR ATM CARD DETAILS', msg)
 }
 
 //payment Confirnmation --------Hope
-function payConfirm(msg){
+function payConfirm(msg) {
      header('YOUR PAYMENT WAS SUCCESSFUL !', msg)
 }
 
